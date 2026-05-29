2f124c61c69c7069c22d0cd4cbecf67b3f69fec4
Lidia Lankova <lankova_lidiya@mail.ru>
2026-05-29 18:28:34 +0300

    Remove abandoned game_2

--- games/game_2_chameleon/ideasforpython.py
+++
@@ -1,7 +0,0 @@
-import random
-
-#список цветов
-colors = ["RED", "BLUE", "ORANGE", "YELLOW"]
-
-color = random.choice(colors)
-print("Our secret word is ", color)
--- games/game_2_chameleon/script.js
+++
@@ -1,5 +0,0 @@
-function func() {
-    var rand = Math.floor(Math.random() * 1000);
-    var location = 'https://english-games.ru/blendin.html' + rand;
-    document.location.href = location
-  }
