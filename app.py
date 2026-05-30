7c3afb0aa24bc22f9aab4a1d083f2e0b8ee494d7
Lidia Lankova <lankova_lidiya@mail.ru>
2026-05-30 16:33:48 +0300

    Change host to 0.0.0.0 for deployment

--- app.py
+++ app.py
@@ -491,4 +491,4 @@ load_rooms_from_db()
 
 if __name__ == '__main__':
     # debug=True auto-restarts when code changes
-    socketio.run(app, debug=True)
+    socketio.run(app, debug=True, host='0.0.0.0', port=5000)
